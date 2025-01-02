3d71af3b54b73cb4094baa71b67d0819d778ea54
Hodaka Mori <hodaka.mori.office@gmail.com>
2025-01-02 20:14:17 +0900

    Update

--- 042_coarse_grained_modelling/forcefield.py
+++ 042_coarse_grained_modelling/forcefield.py
@@ -15,23 +15,29 @@ class CGForceField:
         return cls(param_dict)
 
     def create_system(self, topology: app.Topology) -> openmm.System:
+        """
+        全体の流れ:
+         - system, forces を作成
+         - nonbond, bond, angle, torsion の設定
+         - 1-2,1-3 exclusionを高速に行う
+         - param_dict["ANGLE"] は (already rad, kJ/(mol·rad^2)) など
+         - param_dict["TORSION"] は 単一 or 複数辞書 (list)
+        """
         system = openmm.System()
         atoms = list(topology.atoms())
 
-        # -- Forces
+        # (1) Forces
         bond_force = openmm.HarmonicBondForce()
-        system.addForce(bond_force)
-
         angle_force = openmm.HarmonicAngleForce()
-        system.addForce(angle_force)
-
         torsion_force = openmm.PeriodicTorsionForce()
-        system.addForce(torsion_force)
-
         nonbonded_force = openmm.NonbondedForce()
+
+        system.addForce(bond_force)
+        system.addForce(angle_force)
+        system.addForce(torsion_force)
         system.addForce(nonbonded_force)
 
-        # 1) nonbond params: mass, LJ, charge
+        # (2) Nonbonded particles
         nonbonded_params = self.param_dict.get("NONBONDED", {})
         for i, atom in enumerate(atoms):
             res_label = atom.residue.name
@@ -41,49 +47,51 @@ class CGForceField:
             epsilon_kj = nb_data["epsilon"]  # kJ/mol
             charge_e = nb_data["charge"]  # e
 
-            # mass
             system.addParticle(mass_value * unit.amu)
-            # nonbond (LJ + coulomb)
             nonbonded_force.addParticle(
                 charge_e,
                 sigma_nm * unit.nanometer,
                 epsilon_kj * unit.kilojoule_per_mole,
             )
-            adjacency = defaultdict(set)
 
-            # bond一覧を取得
-            bond_list = list(topology.bonds())
-            # adjacency list
-            for bond in bond_list:
-                a1, a2 = bond
-                i1 = a1.index
-                i2 = a2.index
-                adjacency[i1].add(i2)
-                adjacency[i2].add(i1)
+        # (3) build adjacency for 1-2,1-3 exclusion
+        adjacency = defaultdict(set)
+        bond_list = list(topology.bonds())
+        for bond in bond_list:
+            a1, a2 = bond
+            i1, i2 = a1.index, a2.index
+            adjacency[i1].add(i2)
+            adjacency[i2].add(i1)
 
-            # 1-2: 完全除外
-            for atom1, atom2 in bond_list:
-                i1, i2 = atom1.index, atom2.index
-                # addException( p1, p2, chargeProd, sigma, epsilon, replace=False )
-                # 完全除外 => chargeProd=0.0, epsilon=0.0
-                nonbonded_force.addException(i1, i2, 0.0, 1.0, 0.0, replace=True)
+        # 1-2, 1-3 exclusion in a single pass
+        #  => for i in adjacency:
+        #       for j in adjacency[i]: (j> i) => 1-2
+        #         for k in adjacency[i]: if k> j => 1-3
+        for i in adjacency:
+            neigh_i = sorted(adjacency[i])  # ensure i< j < k if you like
+            for j in neigh_i:
+                if j > i:
+                    # 1-2 => exclude
+                    nonbonded_force.addException(i, j, 0.0, 1.0, 0.0, replace=True)
+            # 1-3
+            #   if i-j and i-k => j,k in adjacency[i]
+            #   exclude i,k
+            #   we do j<k for k>j
+            #   also k != i => trivially, but i is fixed
+            for idx_a in range(len(neigh_i)):
+                j = neigh_i[idx_a]
+                if j <= i:
+                    continue
+                for idx_b in range(idx_a + 1, len(neigh_i)):
+                    k = neigh_i[idx_b]
+                    if k > j:
+                        # 1-3 => exclude
+                        nonbonded_force.addException(i, k, 0.0, 1.0, 0.0, replace=True)
 
-            angle_list = []
-            for j in adjacency:
-                neigh = list(adjacency[j])
-                for i in neigh:
-                    if i < j:
-                        for k in neigh:
-                            if k > j and k != i:
-                                angle_list.append((i, j, k))
-                                # 1-3 => exclude
-                                nonbonded_force.addException(
-                                    i, k, 0.0, 1.0, 0.0, replace=True
-                                )
         nonbonded_force.setNonbondedMethod(openmm.NonbondedForce.CutoffPeriodic)
-        nonbonded_force.setCutoffDistance(1.1)
+        nonbonded_force.setCutoffDistance(1.1 * unit.nanometer)
 
-        # 2) BondForce
+        # (4) BondForce
         bond_params = self.param_dict.get("BOND", {})
         for bond in topology.bonds():
             atom1, atom2 = bond
@@ -101,7 +109,7 @@ class CGForceField:
             elif key2 in bond_params:
                 bp = bond_params[key2]
             else:
-                raise ValueError(f"Unknown bond: {label1}-{label2}")
+                raise ValueError(f"Unknown bond param for: {label1}-{label2}")
 
             r0 = bp["r0"]  # nm
             k_val = bp["k"]  # kJ/(mol·nm^2)
@@ -111,22 +119,19 @@ class CGForceField:
                 r0 * unit.nanometer,
                 k_val * unit.kilojoule_per_mole / (unit.nanometer**2),
             )
-            # system.addConstraint(i1, i2, r0 * unit.nanometer)
 
+        # (5) AngleForce (angles in rad)
         angle_params = self.param_dict.get("ANGLE", {})
-        angles = self._get_angles(topology)  # (i,j,k)
-
+        angles = self._get_angles(topology)
         for i, j, k in angles:
             resA = atoms[i].residue.name
             resB = atoms[j].residue.name
             resC = atoms[k].residue.name
-
             key1 = (resA, resB, resC)
             if key1 in angle_params:
                 ap = angle_params[key1]
-                theta0_rad = ap["theta0"]
-                k_rad = ap["k"]
-
+                theta0_rad = ap["theta0"]  # rad
+                k_rad = ap["k"]  # kJ/(mol·rad^2)
                 angle_force.addAngle(
                     i,
                     j,
@@ -135,33 +140,52 @@ class CGForceField:
                     k_rad * unit.kilojoule_per_mole / (unit.radian**2),
                 )
 
+        # (6) TorsionForce => single or multiple dict
         torsion_params = self.param_dict.get("TORSION", {})
         tors_list = self._get_torsions(topology)
-
         for i, j, k, l in tors_list:
             resA = atoms[i].residue.name
             resB = atoms[j].residue.name
             resC = atoms[k].residue.name
             resD = atoms[l].residue.name
-
             key1 = (resA, resB, resC, resD)
             if key1 in torsion_params:
-                tp = torsion_params[key1]
-                periodicity = tp["periodicity"]
-                phase_rad = tp["phase"]  # already in rad
-                k_tor = tp["k"]  # kJ/mol
+                val = torsion_params[key1]
+                if isinstance(val, dict):
+                    # single dict
+                    periodicity = val["periodicity"]
+                    phase_rad = val["phase"]  # rad
+                    k_tor = val["k"]  # kJ/mol
+                    torsion_force.addTorsion(
+                        i,
+                        j,
+                        k,
+                        l,
+                        periodicity,
+                        phase_rad * unit.radian,
+                        k_tor * unit.kilojoule_per_mole,
+                    )
+                elif isinstance(val, list):
+                    # multiple
+                    for tp in val:
+                        periodicity = tp["periodicity"]
+                        phase_rad = tp["phase"]
+                        k_tor = tp["k"]
+                        torsion_force.addTorsion(
+                            i,
+                            j,
+                            k,
+                            l,
+                            periodicity,
+                            phase_rad * unit.radian,
+                            k_tor * unit.kilojoule_per_mole,
+                        )
+                else:
+                    raise ValueError(
+                        f"TORSION param for {key1} must be dict or list of dict"
+                    )
 
-                torsion_force.addTorsion(
-                    i,
-                    j,
-                    k,
-                    l,
-                    periodicity,
-                    phase_rad * unit.radian,
-                    k_tor * unit.kilojoule_per_mole,
-                )
-
-        # 6) set PBC
+        # (7) PBC
         box_dims = topology.getUnitCellDimensions()
         if box_dims is not None:
             lx, ly, lz = box_dims
@@ -174,12 +198,12 @@ class CGForceField:
 
         return system
 
+    # adjacency builder
     def _build_adjacency_list(self, topology: app.Topology) -> Dict[int, set]:
         adjacency = defaultdict(set)
         for bond in topology.bonds():
             a1, a2 = bond
-            i1 = a1.index
-            i2 = a2.index
+            i1, i2 = a1.index, a2.index
             adjacency[i1].add(i2)
             adjacency[i2].add(i1)
         return adjacency
@@ -188,20 +212,34 @@ class CGForceField:
         adjacency = self._build_adjacency_list(topology)
         angles = []
         for j in adjacency:
-            neighbors_j = list(adjacency[j])
-            for i in neighbors_j:
+            neigh = sorted(adjacency[j])
+            for idx_a in range(len(neigh)):
+                i = neigh[idx_a]
                 if i < j:
-                    for k in neighbors_j:
+                    for idx_b in range(idx_a + 1, len(neigh)):
+                        k = neigh[idx_b]
                         if k > j and k != i:
                             angles.append((i, j, k))
         return angles
 
     def _get_torsions(self, topology: app.Topology) -> List[Tuple[int, int, int, int]]:
         adjacency = self._build_adjacency_list(topology)
-        angles = self._get_angles(topology)  # (i, j, k)
+        # build angles first
+        angles = []
+        for j in adjacency:
+            neigh = sorted(adjacency[j])
+            for idx_a in range(len(neigh)):
+                i = neigh[idx_a]
+                if i < j:
+                    for idx_b in range(idx_a + 1, len(neigh)):
+                        k = neigh[idx_b]
+                        if k > j and k != i:
+                            angles.append((i, j, k))
+
         torsions = []
         for i, j, k in angles:
-            for l in adjacency[k]:
+            neigh_k = adjacency[k]
+            for l in neigh_k:
                 if l > k and l not in (i, j):
                     torsions.append((i, j, k, l))
         return torsions
